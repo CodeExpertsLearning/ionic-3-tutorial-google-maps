7e8fe39ec64e38c66321c17fdc069ddf4144376b
Nanderson Castro <nandokstro@gmail.com>
2017-12-31 19:09:36 -0300

    Adicionando codigos do cel drops.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') map: ElementRef;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewDidLoad() {
+    this.showMap();
+  }
+
+  showMap() {
+    const position = new google.maps.LatLng(-2.6289487, -44.3999958);
+
+    const options = {
+      center: position,
+      zoom: 10,
+      mapTypeId: 'hybrid'
+    };
+
+    const map = new google.maps.Map(this.map.nativeElement, options);
+
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
+  }
 }
